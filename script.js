3e34cddc5e569908d522cbf782f1ca7dbec34ca8
Eniola Daniel <eniolad206@gmail.com.com>
2025-05-13 21:17:52 +0100

    updates

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function toggleMenu() {
   const nav = document.querySelector(".navbar");
   nav.classList.toggle("show");
+  // Close menu when a link is clicked
+  const links = nav.querySelectorAll("a");
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("show");
+    });
+  });
 }
