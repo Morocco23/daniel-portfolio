68af255a6829a88d2f20bf8d69676ce2f2e07d29
Eniola Daniel <eniolad206@gmail.com.com>
2025-05-15 14:18:05 +0100

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+var z = 0;
 function toggleMenu() {
   const nav = document.querySelector(".navbar");
   nav.classList.toggle("show");
+  const menuIcon = document.getElementById("menu-icon");
+  // Change icon to X when menu is open
+  /*if (nav.classList.contains("show")) {
+    menuIcon.innerHTML = `<i>h</i>`;
+  } else {
+    menuIcon.innerHTML = `<i>b</i>`;
+  }*/
+  if (z == 0) {
+    menuIcon.style.transform = "rotate(90deg)";
+    z = 1;
+  } else {
+    menuIcon.style.transform = "rotate(0deg)";;
+    z = 0;
+  }
   // Close menu when a link is clicked
   const links = nav.querySelectorAll("a");
   links.forEach(link => {
